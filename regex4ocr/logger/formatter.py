a93d94e441551f315386c4632f6cf3a1501cf02e
Igor Grillo Peternella <igor.feq@gmail.com>
2019-01-04 15:39:50 -0200

    fix logging message

--- regex4ocr/logger/formatter.py
+++ regex4ocr/logger/formatter.py
@@ -42,7 +42,9 @@ def format_logger(logger):
 
     # sets the logger format handler
     handler.setFormatter(
-        LogFormatter("%(asctime)s - %(name)s - levelee=%(levelname)s - %(message)s")
+        LogFormatter(
+            "%(asctime)s - %(name)s - level=%(levelname)s - %(message)s"
+        )
     )
 
     # sets the logger level based on the os variable
