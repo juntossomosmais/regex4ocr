e9ffbb1632138958569dc1e723238b6608f75916
Igor Grillo Peternella <igor.feq@gmail.com>
2019-01-04 15:44:57 -0200

    fixed logging message

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="regex4ocr",
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/juntossomosmais/regex4ocr",
     license="MIT",
     author="Igor Grillo Peternella",
