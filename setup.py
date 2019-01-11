532f07de559eee484c2f13caa61ddad41178d28a
Igor Grillo Peternella <igor.feq@gmail.com>
2019-01-11 09:26:21 -0200

    added install dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 """
 Setup module to build distribution packages of regex4ocr.
 """
-
 from setuptools import setup
 
 setup(
     name="regex4ocr",
-    version="1.0.2",
+    version="1.0.3",
     url="https://github.com/juntossomosmais/regex4ocr",
     license="MIT",
     author="Igor Grillo Peternella",
     author_email="igor.feq@gmail.com",
     description="Extract data from OCR string results based on Document Regexp Models (DRMs).",
     packages=["regex4ocr", "regex4ocr.logger", "regex4ocr.parser"],
+    install_requires=["PyYAML==3.13", "Unidecode==1.0.23"],
     long_description=open("README.md").read(),
     zip_safe=False,
 )
