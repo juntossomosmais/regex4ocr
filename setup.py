b29309cff2de8f0fe25e6092954a08473b9c9907
Igor Grillo Peternella <igor.feq@gmail.com>
2019-01-11 09:29:31 -0200

    fixed pyaml dependency on setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup
 
 setup(
     name="regex4ocr",
-    version="1.0.3",
+    version="1.0.4",
     url="https://github.com/juntossomosmais/regex4ocr",
     license="MIT",
     author="Igor Grillo Peternella",
     author_email="igor.feq@gmail.com",
     description="Extract data from OCR string results based on Document Regexp Models (DRMs).",
     packages=["regex4ocr", "regex4ocr.logger", "regex4ocr.parser"],
-    install_requires=["PyYAML==3.13", "Unidecode==1.0.23"],
+    install_requires=["PyYAML==4.2b1", "Unidecode==1.0.23"],
     long_description=open("README.md").read(),
     zip_safe=False,
 )
