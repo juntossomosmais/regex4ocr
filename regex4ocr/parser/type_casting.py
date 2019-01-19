07ce54e557c1fdc6dc088674d5e7a8912ca04e80
Ígor Grillo Peternella <igor.feq@gmail.com>
2019-01-19 13:53:56 -0200

    added support for datetime type casts. added unit tests

--- regex4ocr/parser/type_casting.py
+++ regex4ocr/parser/type_casting.py
@@ -2,6 +2,7 @@
 Module with type-casting related functions.
 """
 import logging
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -11,19 +12,35 @@ def validate_types(extracted_data, drm):
     Performs data validation of the extracted_data according to the
     types informed at the drm for this document.
     """
+    logger.info("Beginning types validation...")
+
     fields_types_section = drm.get("types", {}).get("fields", {})
     inline_groups_types_section = (
         drm.get("types", {}).get("table", {}).get("inline_named_group_captures")
     )
     rows = extracted_data.get("table", {}).get("rows")
 
+    logger.info("Performing inline captured groups type casting...")
+
     if rows and inline_groups_types_section:
         # removes named groups if type casting fails
         for row_dict in rows:
+            logger.debug(
+                "Validating row data: %s, with the types: %s",
+                row_dict["data"],
+                inline_groups_types_section,
+            )
+
             remove_wrong_types(row_dict["data"], inline_groups_types_section)
 
     # fields is always required
     if fields_types_section:
+        logger.debug(
+            "Validating fields: %s, with the types: %s",
+            extracted_data["fields"],
+            fields_types_section,
+        )
+
         remove_wrong_types(extracted_data["fields"], fields_types_section)
 
 
@@ -33,8 +50,8 @@ def remove_wrong_types(extracted_data_section, drm_types_section):
     are the extracted_data_section keys and values are the are desired types for
     the extracted_data_section keys.
 
-    If the cast of the extracted_data_section to the desired type fails, that key
-    is POPPED from the original dictionary. Hence, this method MUTATES the
+    If the cast of the extracted_data_section to the desired type fails, that
+    key is POPPED from the original dictionary. Hence, this method MUTATES the
     original extracted_data_section.
 
     Args:
@@ -48,6 +65,9 @@ def remove_wrong_types(extracted_data_section, drm_types_section):
 
         # the type has a matching desired_field
         if extracted_field:
+            logger.debug(
+                "Found cast: %s, to %s...", extracted_field, desired_type
+            )
             cast_rslt = cast_type(extracted_field, desired_type)
 
             if cast_rslt:
@@ -70,7 +90,16 @@ def cast_type(extracted_field, desired_type):
     Returns:
         (desired_type): the extracted_field coerced to the desired_type or None.
                         if there was a coercion error.
+
+    Raises:
+        ValueError: if the extracted_field cannot be cast to the desired type.
+        BaseException: if the DRM contains unknown types.
     """
+    logger.debug("Casting: %s, to %s...", extracted_field, desired_type)
+
+    if isinstance(desired_type, list):
+        return _cast_type_list(extracted_field, desired_type)
+
     types_mapping = {"int": int, "float": float, "str": str}
 
     if desired_type not in types_mapping.keys():
@@ -90,3 +119,52 @@ def cast_type(extracted_field, desired_type):
             desired_type,
         )
         return None
+
+
+def _cast_type_list(extracted_field, desired_type):
+    """
+    Performs data casting when the desired_type is list that contains the
+    type and associated information about it.
+
+    Supported types with metadata:
+        - ['datetime', <datetime_format_string>] --> returns ISO datetime
+
+    Args:
+        extracted_field (str): extract field from the ocr
+        desired_type: (list): type info in the format: [<type>, <type_metadata>]
+
+    Returns:
+        (desired_type): the extracted_field coerced to the desired_type or None.
+                        if there was a coercion error.
+
+    Raises:
+        ValueError: if the extracted_field cannot be cast to the desired type.
+        BaseException: if the DRM contains unknown types.
+    """
+    logger.debug(
+        "Casting type with metadata: %s, to %s...",
+        extracted_field,
+        desired_type,
+    )
+
+    supported_types = ("datetime",)
+
+    if len(desired_type) < 2 or desired_type[0] not in supported_types:
+        raise BaseException(
+            "Unknown specified type at the DRM: %s", desired_type
+        )
+
+    if desired_type[0] == "datetime":
+        try:
+
+            return datetime.strptime(
+                extracted_field, desired_type[1]
+            ).isoformat()
+
+        except ValueError:
+            logger.info(
+                "Removed wrong type data: %s, desired_type: %s",
+                extracted_field,
+                desired_type,
+            )
+            return None
--- tests/integration/test_type_casting.py
+++ tests/integration/test_type_casting.py
@@ -1,15 +1,15 @@
 """
 Module with unit tests for the data type casting functions
 """
-import re
+import copy
 from unittest import mock
 
 import pytest
+
 from regex4ocr.parser.type_casting import cast_type
 from regex4ocr.parser.type_casting import remove_wrong_types
 from regex4ocr.parser.type_casting import validate_types
 from regex4ocr.parser.yml_parser import parse_yml
-
 from tests.data.aux import open_file
 
 
@@ -40,6 +40,7 @@ def extracted_data_dict_1():
     return {
         "fields": {
             "cnpj": "10.549.937/000174",
+            "date": "10/08/1991",
             "coo": "047621",
             "some_int": "1234",
         },
@@ -76,6 +77,7 @@ def expected_extracted_data_dict_no_removal():
         "fields": {
             "cnpj": "10.549.937/000174",
             "coo": "047621",
+            "date": "1991-08-10T00:00:00",  # cast to iso format
             "some_int": 1234,  # casting
         },
         "table": {
@@ -112,6 +114,7 @@ def expected_extracted_data_dict_with_removal():
             "cnpj": "10.549.937/000174",
             "coo": "047621",
             # "some_int": "1234",
+            # "date": "10/08/1991",
         },
         "table": {
             "header": "iten codigo descricao qid1un vl1unit r$ ) st vl item(r$)",
@@ -140,61 +143,101 @@ def expected_extracted_data_dict_with_removal():
     }
 
 
-def test_cast_type_exception_unknown_drm_type(
-    extracted_data_dict_1, drm_model_tax_coupon_with_inline_groups
-):
+def test_cast_type_exception_unknown_drm_type():
     """
     Unit: tests cast_type when there's an unsupported or unknown type at the
           DRM yml file.
     """
     with pytest.raises(BaseException):
-        cast_type(extracted_data_dict_1["fields"]["some_int"], "unknown_type")
+        cast_type("1234", "unknown_type")
 
 
-def test_cast_type_int_type(
-    extracted_data_dict_1, drm_model_tax_coupon_with_inline_groups
-):
+def test_cast_type_int_type():
     """
     Unit: tests cast_type when there's a correct cast to an int type.
     """
-    assert cast_type(extracted_data_dict_1["fields"]["some_int"], "int") == 1234
+    assert cast_type("1234", "int") == 1234
 
 
-def test_cast_type_value_exception(
-    extracted_data_dict_1, drm_model_tax_coupon_with_inline_groups
-):
+def test_cast_type_datetime_type_exception():
     """
-    Unit: tests cast_type when there's a ValueError is raised.
+    Unit: tests cast_type when there's an impossible cast to datetime.
     """
-    assert cast_type(extracted_data_dict_1["fields"]["cnpj"], "int") is None
+    assert cast_type("10/08/1991", ["datetime", "abc"]) is None
+
+
+def test_cast_type_datetime_type_ok():
+    """
+    Unit: tests cast_type when there's a correct cast to a datetime type.
+    """
+    assert (
+        cast_type("10/08/1991", ["datetime", "%d/%m/%Y"])
+        == "1991-08-10T00:00:00"
+    )
+
+
+def test_cast_type_datetime_wrong_list_length():
+    """
+    Unit: tests cast_type when there's missing datetime metadata
+          for the type cast.
+    """
+    with pytest.raises(BaseException):
+        cast_type("10/08/1991", ["datetime"])
+
+
+def test_cast_type_datetime_no_timestamp():
+    """
+    Unit: tests cast_type when the datetime metadata contains no
+          timestamp info.
+    """
+    assert cast_type("10/08/1991", ["datetime", "%d/%m/%Y %H:%M:%S"]) is None
+
+
+def test_cast_type_metadata_unknown():
+    """
+    Unit: tests cast_type when the desired type is a list with unknown type.
+    """
+    with pytest.raises(BaseException):
+        cast_type("10/08/1991", ["unknown_type", "whatever"])
+
+
+def test_cast_type_value_exception():
+    """
+    Unit: tests cast_type when there's raised ValueError.
+    """
+    assert cast_type("123.144-5566/12345-6", "int") is None
 
 
 def test_remove_wrong_types_no_removal(
     extracted_data_dict_1, drm_model_tax_coupon_with_inline_groups
 ):
     """
-    Unit: tests cast_type when there's an unsupported or unknown type at the
-          DRM yml file.
+    Unit: tests cast_type when all the type casts run smoothly.
     """
-    extracted_data_section = extracted_data_dict_1["fields"]
+    # sends only the fields section
+    extracted_data_section = {
+        "cnpj": "10.549.937/000174",
+        "coo": "047621",
+        "date": "10/08/1991",
+        "some_int": "1234",
+    }
 
+    # types > fields
     drm_types_section = {
-        "types": {
-            "fields": {
-                "cnpj": "str",
-                # "coo": "047621", # not inserted
-                "some_int": "int",
-            }
-        }
+        "cnpj": "str",
+        # "coo": "047621", # not inserted
+        "some_int": "int",
+        "date": ["datetime", "%d/%m/%Y"],
     }
 
     expected_extracted_data_dict = {
         "cnpj": "10.549.937/000174",
-        "coo": "047621",
-        "some_int": "1234",
+        "coo": "047621",  # since there was no typing, remains as is
+        "date": "1991-08-10T00:00:00",
+        "some_int": 1234,
     }
 
-    # function invocation
+    # function invocation which mutates extracted_data_section
     remove_wrong_types(extracted_data_section, drm_types_section)
     assert extracted_data_section == expected_extracted_data_dict
 
@@ -210,9 +253,15 @@ def test_remove_wrong_types_with_removal(
         "cnpj": "10.549.937/000174",
         "coo": "047621",
         "some_int": "1234xxnotanint",  # not an integer
+        "date": "123/10/2018",  # broken date
     }
 
-    drm_types_section = {"cnpj": "str", "coo": "str", "some_int": "int"}
+    drm_types_section = {
+        "cnpj": "str",
+        "coo": "str",
+        "some_int": "int",
+        "date": ["datetime", "%d/%m/%Y"],
+    }
 
     expected_extracted_data_dict = {
         "cnpj": "10.549.937/000174",
@@ -251,11 +300,12 @@ def test_validate_types_removal(
     Unit: tests validate_types when there are type conflicts and
           some fields are removed.
     """
-    extracted_data_dict_1["fields"]["some_int"] = "abc123"  # messes int casting
+    # creates a deep copy of the fixture so mutations wont affect next tests
+    extracted_data_dict = copy.deepcopy(extracted_data_dict_1)
+
+    extracted_data_dict["fields"]["some_int"] = "abc123"  # messes int casting
 
     # function invocation
-    validate_types(
-        extracted_data_dict_1, drm_model_tax_coupon_with_inline_groups
-    )
+    validate_types(extracted_data_dict, drm_model_tax_coupon_with_inline_groups)
 
-    assert extracted_data_dict_1 == expected_extracted_data_dict_with_removal
+    assert extracted_data_dict == expected_extracted_data_dict_with_removal
